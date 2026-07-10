d3c85f70bef53ad049951df2aab9d7e54468c2ce
joesef127 <adegboladayor@gmail.com>
2026-07-10 18:39:35 +0100

    feat: added shadcn and lucide

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import type { Metadata } from "next";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full antialiased">
+    <html lang="en" className={cn("h-full antialiased")} suppressHydrationWarning={true}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
